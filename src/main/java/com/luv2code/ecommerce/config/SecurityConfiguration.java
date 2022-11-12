df3fb4cd4cf66e8f6f6328eef3afe23c93837c68
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-11-12 09:10:25 -0300

    Security Order-History no backend

---
+++ src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.luv2code.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // protegendo endpoint /api/orders
+        http.authorizeRequests()
+                        .antMatchers("/api/orders/**")
+                        .authenticated()
+                        .and()
+                        .oauth2ResourceServer()
+                        .jwt();
+        // add cors filters
+        http.cors();
+
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        Okta.configureResourceServer401ResponseBody(http);
+
+        // desabilitando o CSRF
+        http.csrf().disable();
+
+        return http.build();
+    }
+}
